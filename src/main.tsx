0b2449dd44b7a0e0b743aaac198eea5ec20398c8
Hosuu <48166166+Hosuu@users.noreply.github.com>
2022-12-06 17:19:55 +0100

    chromatic

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { CSSvariables } from './styles/CSSvariables'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { ResetCSS } from './styles/ResetCSS'
 
+//Chromatic pls work uwu
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<ResetCSS />
